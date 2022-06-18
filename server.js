525662d0d14b90c82271c95560cf3a42b6754a82
Yurippe <yurichikawa1992@gmail.com>
2022-06-18 00:51:01 -0400

    add user routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const app = require('express').express();
+const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/yurillium-sns-api');
 
